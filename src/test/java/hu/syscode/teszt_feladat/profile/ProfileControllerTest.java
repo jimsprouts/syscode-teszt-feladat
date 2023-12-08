8092d3171f91ba26169b0a39663bb5f45aa3bbc3
Jim Sprouts <jimsprouts@gmail.com>
2023-12-08 03:54:15 +0100

    Added more tests

--- src/test/java/hu/syscode/teszt_feladat/profile/ProfileControllerTest.java
+++ src/test/java/hu/syscode/teszt_feladat/profile/ProfileControllerTest.java
@@ -1,36 +1,57 @@
 package hu.syscode.teszt_feladat.profile;
 
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
+import java.util.UUID;
+
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 
+import lombok.extern.slf4j.Slf4j;
+
 @SpringBootTest
 @AutoConfigureMockMvc
-public class ProfileControllerIT {
+@Slf4j
+public class ProfileControllerTest {
 
     @Autowired
     private MockMvc mockMvc;
+
+    @Autowired
+    public ProfileController profileController;
+
+    @MockBean
+    public StudentService studentService;
 
     private ObjectMapper objectMapper = new ObjectMapper();
 
     @Test
     public void testNewStudent() throws Exception {
-        StudentDto studentDto = StudentDto.builder().name("Mariah Carey").email("mariah.carey@mariahcarey.com").build();
+        String newId = UUID.randomUUID().toString();
+        log.debug("Creating new student with id " + newId);
+        
+        StudentDto inputStudentDto = StudentDto.builder().name("Mariah Carey").email("mariah.carey@mariahcarey.com").build();
+        StudentDto outputStudentDto = StudentDto.builder().id(newId).name("Mariah Carey").email("mariah.carey@mariahcarey.com").build();
+
+        when(studentService.save(any())).thenReturn(outputStudentDto);
 
         mockMvc.perform(post("/students").contentType(MediaType.APPLICATION_JSON_VALUE)
-                .content(objectMapper.writeValueAsBytes(studentDto)))
+                .content(objectMapper.writeValueAsBytes(inputStudentDto)))
                 .andDo(print())
                 .andExpect(status().isOk())
+                .andExpect(jsonPath("$.id").value(newId))
                 .andExpect(jsonPath("$.name").value("Mariah Carey"));
     }
 }
--- src/test/java/hu/syscode/teszt_feladat/profile/StudentServiceTest.java
+++ src/test/java/hu/syscode/teszt_feladat/profile/StudentServiceTest.java
@@ -1,6 +1,44 @@
 package hu.syscode.teszt_feladat.profile;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.verify;
 
+import java.util.UUID;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.Spy;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+@ExtendWith(MockitoExtension.class)
 public class StudentServiceTest {
+
+    @InjectMocks
+    private StudentService studentService;
+
+    @Mock
+    private StudentRepository studentRepository;
+
+    @Spy
+    private StudentMapper studentMapper = new StudentMapperImpl();
+
+    @Test
+    public void testSave() {
+        UUID newId = UUID.randomUUID();
+        StudentDto studentDto = StudentDto.builder().name("Mariah Carey").email("mariah.carey@mariahcarey.com").build();
+        Student student = Student.builder().id(newId).name("Mariah Carey").email("mariah.carey@mariahcarey.com").build();
+
+        Mockito.when(studentRepository.save(any())).thenReturn(student);
+
+        StudentDto newStudentDto = studentService.save(studentDto);
+
+        verify(studentRepository).save(any());
+        assertEquals(newId.toString(), newStudentDto.getId());
+
+    }
 
 }
